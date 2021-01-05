12a5e428da8623529467c6ae42df98cd2fab46b9
vanalex <apjorquera@gmail.com>
2021-01-05 22:50:22 +0100

    remove js files

--- basics.js
+++
@@ -1,8 +0,0 @@
-var isDone = false;
-console.log(isDone);
-var employeeName = "John";
-var color = "blue";
-var fullName = "Bob Bobbington";
-var age = 37;
-var sentence = "Hello, my name is " + fullName + " 'll be " + (age + 1) + " years old next month.";
-console.log(sentence);
--- label.js
+++
@@ -1,5 +0,0 @@
-function printLabel(labeledObj) {
-    console.log(labeledObj.label);
-}
-var myObj = { label: "Size 10 Object" };
-printLabel(myObj);
